a725a4a7c17238f19cdb57ae548e4d3a8e87a985
Bavo27 <bvandenh@macalester.edu>
2025-04-25 11:05:01 -0400

    Team comparing

---
+++ src/generalManager/TeamComparator.java
@@ -0,0 +1,16 @@
+package generalManager;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class TeamComparator implements Comparator<Team> {
+    
+    private String name;
+    private List<Player> players;
+
+    @Override
+    public int compare(Team o1, Team o2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+    }
+}
