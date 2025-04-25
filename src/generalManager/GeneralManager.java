9a5cfa4ca681c3eee64c72df5c7f8b3af9ab7361
Bavo27 <bvandenh@macalester.edu>
2025-04-25 11:40:29 -0400

    general manager class

--- src/generalManager/GeneralManager.java
+++ src/generalManager/GeneralManager.java
@@ -1,5 +1,18 @@
 package generalManager;
 
-public class GeneralManager {
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.ui.Button;
 
+public class GeneralManager {
+    public CanvasWindow canvas;
+    public GeneralManager() {
+        canvas = new CanvasWindow("SoccerGM", 1920, 1080);
+        Button startButton = new Button("Play");
+        canvas.add(startButton);
+        canvas.draw();
+        startButton.onClick(() -> selectingScreen());
+    }
+    public void selectingScreen() {
+        canvas.removeAll();
+    }
 }
