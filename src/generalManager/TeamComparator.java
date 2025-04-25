62d9f25628e94d2d91ad9db8d6b9209def22d988
Gunnar Ray <gunnarsray@gmail.com>
2025-04-25 10:10:11 -0500

    Quick change

--- src/generalManager/TeamComparator.java
+++ src/generalManager/TeamComparator.java
@@ -1,12 +1,8 @@
 package generalManager;
 
 import java.util.Comparator;
-import java.util.List;
 
 public class TeamComparator implements Comparator<Team> {
-    
-    private String name;
-    private List<Player> players;
 
     @Override
     public int compare(Team o1, Team o2) {
